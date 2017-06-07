55f4fbee47285bdc5e1506211013fd102fc38cf7
robmarkcole <robmarkcole@gmail.com>
2017-06-07 07:19:11 +0100

    MQTT

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ import time
 import pycom
 from machine import Pin
 from dth import DTH
+from simple import MQTTClient
+#
+def settimeout(duration):
+   pass
+#
+client = MQTTClient(client_id="wipy_client", server='192.168.0.100', port=1883)  #
+
+client.settimeout = settimeout
+client.connect()
 
 pycom.heartbeat(False)
 pycom.rgbled(0x000008) # blue
 
 def pin_handler(arg):
     print("Motion detected")
+    client.publish("wipy/motion", "Motion detected")
     pycom.rgbled(0x7f0000) # red
     time.sleep_ms(500)
     pycom.rgbled(0x7f7f00) # yellow
 
 pir = Pin('G4',mode=Pin.IN,pull=Pin.PULL_UP)
-pir.callback(Pin.IRQ_FALLING | Pin.IRQ_RISING, pin_handler)
+pir.callback(Pin.IRQ_RISING, pin_handler)    # Pin.IRQ_FALLING | # Only detect on rising
 
 th = DTH(Pin('P3', mode=Pin.OPEN_DRAIN),0)
 time.sleep(2)
